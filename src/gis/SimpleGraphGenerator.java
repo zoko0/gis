36adb14acb3d200b0326a375d29ff55ea8642852
zoko0 <bartosz.ciulkin@gmail.com>
2017-01-11 17:34:36 +0100

    Formatownie pliku

--- src/gis/SimpleGraphGenerator.java
+++ src/gis/SimpleGraphGenerator.java
@@ -5,7 +5,8 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SimpleGraphGenerator {
-	public static void doGenerate(SimpleDirectedWeightedGraph	<Integer, DefaultWeightedEdge> lGraph, int verCount, double probEdge){	
+	public static void doGenerate(SimpleDirectedWeightedGraph
+				      <Integer, DefaultWeightedEdge> lGraph, int verCount, double probEdge){	
 		Random random = new Random();
 		int randomWeight;
 		int minWeight = 0;
@@ -18,20 +19,18 @@ public class SimpleGraphGenerator {
 		
 		//dodaj krawedzie
 		for (int i = 0; i < verCount; i++) {
-            for (int j = i + 1; j < verCount; j++) {
-            	
-            	randomWeight = ThreadLocalRandom.current().nextInt(minWeight, maxWeight + 1);
-            	System.out.println(randomWeight);
-            	
-            	double randomProb = random.nextDouble();
-                if (randomProb <= probEdge) {
-                	DefaultWeightedEdge edge = lGraph.addEdge(i, j); //zrodlo,cel
-    				lGraph.setEdgeWeight(edge, Double.valueOf(randomWeight)); //dodanie wagi do krawedzi    	                }
-                }
-            }
+		    for (int j = i + 1; j < verCount; j++) {
+
+			randomWeight = ThreadLocalRandom.current().nextInt(minWeight, maxWeight + 1);
+			System.out.println(randomWeight);
+
+			double randomProb = random.nextDouble();
+			if (randomProb <= probEdge) {
+				DefaultWeightedEdge edge = lGraph.addEdge(i, j); //zrodlo,cel
+					lGraph.setEdgeWeight(edge, Double.valueOf(randomWeight)); //dodanie wagi do krawedzi
+			}
+		    }
 		}
 	}
-	
-	
 } //end of class
 
